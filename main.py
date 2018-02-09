c5e39ba604315de3e74fb638b9577acf0743aef5
maxf9 <maxim.yuschenko1@gmail.com>
2018-02-09 13:48:52 +0700

    add interpreter version check

---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+if sys.version_info < (3,6):
+	print("Interpreter error. Use Python interpreter version 3.6 or greater")
+	sys.exit(1)
+
+if __name__ == "__main__":
+	print("OK")

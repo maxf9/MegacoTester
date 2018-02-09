e4c4be55bd6b3257aaf79f2b49be507810845642
maxf9 <maxim.yuschenko1@gmail.com>
2018-02-09 15:30:18 +0700

    completed arg_parser

--- main.py
+++ main.py
@@ -4,5 +4,13 @@ if sys.version_info < (3,6):
 	print("Interpreter error. Use Python interpreter version 3.6 or greater")
 	sys.exit(1)
 
+from parser.arg_parser import ArgParser
+from parser.config_parser import ConfigParser
+from parser.test_parser import TestParser
+
+def main():
+	#Парсинг аргументов командной строки
+	config_file, tests_files = ArgParser().parse_arguments()
+
 if __name__ == "__main__":
-	print("OK")
+	main()
--- parser/arg_parser.py
+++ parser/arg_parser.py
@@ -1,2 +1,23 @@
+from argparse import ArgumentParser
+
 class ArgParser:
-	pass
+
+	_instance = None
+	_arg_parser = ArgumentParser()
+
+	def __new__(cls):
+		if ArgParser._instance is None:
+			ArgParser._instance = object.__new__(cls)
+		return ArgParser._instance
+
+	def __init__(self):
+		ArgParser._define_args()
+
+	@classmethod
+	def _define_args(cls):
+		cls._arg_parser.add_argument("-c", "--config", action="store", type=str, dest="config", required=True)
+		cls._arg_parser.add_argument("-t", "--tests", action="store", nargs="+", type=str, dest="tests", required=True)
+
+	def parse_arguments(self):
+		namespace = ArgParser._arg_parser.parse_args()
+		return (namespace.config, namespace.tests)
